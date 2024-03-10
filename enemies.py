444d47b22920a7185979656d18dd6edb8ded8493
WeenaWibowo <weenawibowo@hotmail.com>
2024-03-09 20:50:41 -0800

    shoots bullets

--- enemies.py
+++ enemies.py
@@ -41,23 +41,36 @@ class player(object):
         self.left = False
         self.right = False
         self.walkCount = 0
+        self.standing = True
 
     def draw(self):  # Removed self.win from parameters
         if self.walkCount + 1 >= 9:
             self.walkCount = 0
 
-        if self.left:
-            win.blit(walkLeft[self.walkCount // 3], (self.x, self.y))  # Changed to self.x, self.y
-            self.walkCount += 1
-        elif self.right:
-            win.blit(walkRight[self.walkCount // 3], (self.x, self.y))  # Changed to self.x, self.y
-            self.walkCount += 1
+        if not(self.standing):
+            if self.left:
+                win.blit(walkLeft[self.walkCount // 3], (self.x, self.y)) 
+                self.walkCount += 1
+            elif self.right:
+                win.blit(walkRight[self.walkCount // 3], (self.x, self.y))  
+                self.walkCount += 1
         else:
-            win.blit(USER_IMG, (self.x, self.y))  # Changed to self.x, self.y
+            if self.right:
+                win.blit(walkRight[0], (self.x,self.y))
+            else:
+                win.blit(walkLeft[0], (self.x,self.y))
 
+class shoot(object):
+    def __init__(self,x,y,radius,color,facing):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.vel = 8 * facing
 
-
-
+    def draw(win):
+        pygame.draw.circle(win, self.color, (self.x,self.y), self.radius) 
 
 
 clock = pygame.time.Clock()
@@ -67,6 +80,8 @@ exit = False
 def redrawScreen():  # Removed unused images parameter
     win.blit(BACKDROP, (0, 0))
     man.draw()  # Changed to call draw on the instance
+    for bullet in bullets:
+        bullet.draw()
     pygame.display.update()
 
 
@@ -78,26 +93,42 @@ class enemy(object):
 # LOOP
 
 man = player(250, 250, 80, 80)
+bullets = []
 while not exit:
     clock.tick(27)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
+    for bullet in bullets:
+        if bullet.x < 500 and bullet.x > 0:
+            bullet.x += bullet.vel 
+        else:
+            bullets.pop(bullets.index(bullet))
+
     keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_SPACE]:
+        if man.left:
+            facing = -1
+        else:
+            facing = 1
+        if len(bullets) < 5:
+            bullets.append(shoot(round(man.x + man.width // 2),round(man.y + man.height // 2), 6, (0,0,0), facing))
     if keys[pygame.K_LEFT] and man.x > man.vel:
         man.x -= man.vel
         man.left = True
         man.right = False
+        man.standing = False
     elif keys[pygame.K_RIGHT] and man.x < 500-man.width-man.vel:
         man.x += man.vel
         man.right = True
         man.left = False
+        man.standing = False
     else:
-        man.right = False
-        man.left = False
+        man.standing = True
         man.walkCount = 0
     if not(man.isJump):
-        if keys[pygame.K_SPACE]:
+        if keys[pygame.K_UP]:
             man.isJump = True
             man.right = False
             man.left = False
