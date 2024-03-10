d4073fea17233f66a9083d5fd187b49e2b7c3856
Undiluted7027 <undiluted7027@protonmail.com>
2024-03-09 20:19:10 -0800

    Merge code refactor and #4

--- enemies.py
+++ enemies.py
@@ -2,11 +2,32 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
+win = pygame.display.set_mode((680, 500))
 pygame.display.set_caption("Mind Guardian 👼")
 
-walkLeft = [pygame.image.load('images/L1.png'), pygame.image.load('images/L2.png'), ...]
-walkRight = [pygame.image.load('images/R1.png'), pygame.image.load('images/R2.png'), ...]
+ST_ENEMY_IMG = pygame.image.load('enemy.png')
+USER_IMG = pygame.image.load('user.png')
+USER_IMG = pygame.transform.scale(USER_IMG, (100, 100))
+BACKDROP = pygame.image.load('images/backdrop4.jpg')
+BACKDROP = pygame.transform.scale(BACKDROP, (680, 500))
+walkLeft = [pygame.image.load('trial/L1.png'), pygame.image.load('trial/L2.png'),pygame.image.load('trial/L3.png')]
+walkRight = [pygame.image.load('trial/R1.png'), pygame.image.load('trial/R2.png'), pygame.image.load('trial/R3.png')]
+walkLeft = [pygame.transform.scale(i, (80, 80)) for i in walkLeft]
+walkRight = [pygame.transform.scale(i, (80, 80)) for i in walkRight]
+
+
+user_x = 0
+user_y = 350
+enemy_x = 350
+enemy_y = 150
+width = 40
+height = 60
+vel = 5
+isJump = False
+jumpCount = 10
+LEFT = False
+RIGHT = False
+walkCount = 0
 
 class player(object):
     def __init__(self, x, y, width, height):
@@ -22,7 +43,7 @@ class player(object):
         self.walkCount = 0
 
     def draw(self):  # Removed self.win from parameters
-        if self.walkCount + 1 >= 27:
+        if self.walkCount + 1 >= 9:
             self.walkCount = 0
 
         if self.left:
@@ -32,21 +53,10 @@ class player(object):
             win.blit(walkRight[self.walkCount // 3], (self.x, self.y))  # Changed to self.x, self.y
             self.walkCount += 1
         else:
-            win.blit(images[0], (self.x, self.y))  # Changed to self.x, self.y
+            win.blit(USER_IMG, (self.x, self.y))  # Changed to self.x, self.y
 
 
-user_x = 0
-user_y = 350
-enemy_x = 350
-enemy_y = 350
-width = 40
-height = 60
-vel = 5
-isJump = False
-jumpCount = 10
-LEFT = False
-RIGHT = False
-walkCount = 0
+
 
 
 
@@ -55,29 +65,31 @@ clock = pygame.time.Clock()
 exit = False
 
 def redrawScreen():  # Removed unused images parameter
-    win.blit(backdrop, (0, 0))
+    win.blit(BACKDROP, (0, 0))
     man.draw()  # Changed to call draw on the instance
     pygame.display.update()
 
 
 class enemy(object):
-    walkRight = [pygame.image.load("images/R1E.png"),pygame.image.load("images/R1E.png"),pygame.image.load("images/R1E.png"),]
+    enemy_walk_left = [pygame.image.load('images/L1E.png'),pygame.image.load('images/L2E.png'),pygame.image.load('images/L3E.png'),pygame.image.load('images/L4E.png'),pygame.image.load('images/L5E.png'),pygame.image.load('images/L6E.png'),pygame.image.load('images/L7E.png'),pygame.image.load('images/L8E.png'),pygame.image.load('images/L9E.png'),pygame.image.load('images/L10E.png'),pygame.image.load('images/L11E.png')]
+    enemy_walk_right = [pygame.image.load('images/R1E.png'),pygame.image.load('images/R2E.png'),pygame.image.load('images/R3E.png'),pygame.image.load('images/R4E.png'),pygame.image.load('images/R5E.png'),pygame.image.load('images/R6E.png'),pygame.image.load('images/R7E.png'),pygame.image.load('images/R8E.png'),pygame.image.load('images/R9E.png'),pygame.image.load('images/R10E.png'),pygame.image.load('images/R11E.png')]
+
 
 # LOOP
 
-man = player(300, 410, 64, 64)
+man = player(250, 250, 80, 80)
 while not exit:
     clock.tick(27)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit = True
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and man.user_x > man.vel:
-        man.user_x -= man.vel
+    if keys[pygame.K_LEFT] and man.x > man.vel:
+        man.x -= man.vel
         man.left = True
         man.right = False
-    elif keys[pygame.K_RIGHT] and man.user_x < 500-man.width-man.vel:
-        man.user_x += man.vel
+    elif keys[pygame.K_RIGHT] and man.x < 500-man.width-man.vel:
+        man.x += man.vel
         man.right = True
         man.left = False
     else:
@@ -92,7 +104,7 @@ while not exit:
             man.walkCount = 0
     else:
         if man.jumpCount >= -10:
-            man.user_y -= (man.jumpCount * abs(man.jumpCount)) * 0.5
+            man.y -= (man.jumpCount * abs(man.jumpCount)) * 0.25
             man.jumpCount -= 1
         else: 
             man.jumpCount = 10
