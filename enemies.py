e37f965ce8b4bb94ff295aaebd0834aba56812c3
Undiluted7027 <undiluted7027@protonmail.com>
2024-03-09 22:11:32 -0800

    Avoid catastrophe

--- enemies.py
+++ enemies.py
@@ -2,7 +2,7 @@ import pygame
 
 pygame.init()
 
-win = pygame.display.set_mode((680, 500))
+win = pygame.display.set_mode((500, 480))
 pygame.display.set_caption("Mind Guardian 👼")
 
 ST_ENEMY_IMG = pygame.image.load('enemy2.png')
@@ -16,19 +16,19 @@ walkRight = [pygame.image.load('trial/R1.png'), pygame.image.load('trial/R2.png'
 walkLeft = [pygame.transform.scale(i, (80, 80)) for i in walkLeft]
 walkRight = [pygame.transform.scale(i, (80, 80)) for i in walkRight]
 
-
-user_x = 0
-user_y = 350
-enemy_x = 350
-enemy_y = 150
-width = 40
-height = 60
-vel = 5
-isJump = False
-jumpCount = 10
-LEFT = False
-RIGHT = False
-walkCount = 0
+clock = pygame.time.Clock()
+# user_x = 0
+# user_y = 550
+# enemy_x = 550
+# enemy_y = 150
+# width = 40
+# height = 60
+# vel = 5
+# isJump = False
+# jumpCount = 10
+# LEFT = False
+# RIGHT = False
+# walkCount = 0
 
 class player(object):
     def __init__(self, x, y, width, height):
@@ -43,48 +43,23 @@ class player(object):
         self.right = False
         self.walkCount = 0
         self.standing = True
-        self.standing = True
 
-    def draw(self, win):  # Removed self.win from parameters
+    def draw(self, win):
         if self.walkCount + 1 >= 9:
             self.walkCount = 0
 
         if not(self.standing):
             if self.left:
-                win.blit(walkLeft[self.walkCount // 3], (self.x, self.y)) 
+                win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
                 self.walkCount += 1
             elif self.right:
-                win.blit(walkRight[self.walkCount // 3], (self.x, self.y))  
-                self.walkCount += 1
-        if not(self.standing):
-            if self.left:
-                win.blit(walkLeft[self.walkCount // 3], (self.x, self.y)) 
-                self.walkCount += 1
-            elif self.right:
-                win.blit(walkRight[self.walkCount // 3], (self.x, self.y))  
-                self.walkCount += 1
+                win.blit(walkRight[self.walkCount//3], (self.x,self.y))
+                self.walkCount +=1
         else:
             if self.right:
-                win.blit(walkRight[0], (self.x,self.y))
+                win.blit(walkRight[0], (self.x, self.y))
             else:
-                win.blit(walkLeft[0], (self.x,self.y))
-
-class shoot(object):
-    def __init__(self,x,y,radius,color,facing):
-        self.x = x
-        self.y = y
-        self.radius = radius
-        self.color = color
-        self.facing = facing
-        self.vel = 8 * facing
-
-    def draw(win):
-        pygame.draw.circle(win, self.color, (self.x,self.y), self.radius) 
-            if self.right:
-                win.blit(walkRight[0], (self.x,self.y))
-            else:
-                win.blit(walkLeft[0], (self.x,self.y))
-
+                win.blit(walkLeft[0], (self.x, self.y))
 
 class shoot(object):
     def __init__(self,x,y,radius,color,facing):
@@ -97,25 +72,24 @@ class shoot(object):
 
     def draw(self, win):
         pygame.draw.circle(win, self.color, (self.x,self.y), self.radius) 
+        
 
-
-
-clock = pygame.time.Clock()
 
 exit = False
 
 def redrawScreen():  # Removed unused images parameter
     win.blit(BACKDROP, (0, 0))
     man.draw(win)  # Changed to call draw on the instance
-    en.draw(win)
+    goblin.draw(win)
     for bullet in bullets:
         bullet.draw(win)
     pygame.display.update()
 
 
 class enemy(object):
-    # enemy_walk_left = [pygame.image.load('')]
-    # enemy_walk_right = [pygame.image.load('images/R1E.png'),pygame.image.load('images/R2E.png'),pygame.image.load('images/R3E.png'),pygame.image.load('images/R4E.png'),pygame.image.load('images/R5E.png'),pygame.image.load('images/R6E.png'),pygame.image.load('images/R7E.png'),pygame.image.load('images/R8E.png'),pygame.image.load('images/R9E.png'),pygame.image.load('images/R10E.png'),pygame.image.load('images/R11E.png')]
+    enemy_walk_left = [pygame.image.load('images/L1E.png'),pygame.image.load('images/L2E.png'),pygame.image.load('images/L3E.png'),pygame.image.load('images/L4E.png'),pygame.image.load('images/L5E.png'),pygame.image.load('images/L6E.png'),pygame.image.load('images/L7E.png'),pygame.image.load('images/L8E.png'),pygame.image.load('images/L9E.png'),pygame.image.load('images/L10E.png'),pygame.image.load('images/L11E.png')]
+    
+    enemy_walk_right = [pygame.image.load('images/R1E.png'),pygame.image.load('images/R2E.png'),pygame.image.load('images/R3E.png'),pygame.image.load('images/R4E.png'),pygame.image.load('images/R5E.png'),pygame.image.load('images/R6E.png'),pygame.image.load('images/R7E.png'),pygame.image.load('images/R8E.png'),pygame.image.load('images/R9E.png'),pygame.image.load('images/R10E.png'),pygame.image.load('images/R11E.png')]
 
     def __init__(self, x, y, width, height, end):
         self.x = x
@@ -123,7 +97,7 @@ class enemy(object):
         self.width = width
         self.height = height
         self.end = end
-        self.path = [self.x, self.end]
+        self.path = [x, end]
         self.walkCount = 0
         self.vel = 3
     
@@ -131,7 +105,7 @@ class enemy(object):
         self.move()
         if self.walkCount + 1 >= 33:
             self.walkCount = 0
-        if vel > 0:
+        if self.vel > 0:
             win.blit(ST_ENEMY_IMG, (self.x, self.y))
             self.walkCount += 1
         else:
@@ -140,14 +114,14 @@ class enemy(object):
 
     def move(self):
         if self.vel > 0:
-            if self.x < self.vel + self.path[1]:
+            if self.x < self.path[1] + self.vel:
                 self.x += self.vel
             else:
                 self.vel = self.vel * -1
                 self.x += self.vel
                 self.walkCount = 0
         else:
-            if self.x > self.vel - self.path[0]:
+            if self.x > self.path[0]-self.vel:
                 self.x += self.vel
             else:
                 self.vel = self.vel * -1
@@ -157,10 +131,10 @@ class enemy(object):
 
 
 # LOOP
-en = enemy(100, 450, 200, 200, 300)
-man = player(250, 250, 80, 80)
+man = player(200, 200, 64,64)
+goblin = enemy(100, 150, 64, 64, 300)
 bullets = []
-bullets = []
+
 while not exit:
     clock.tick(27)
     for event in pygame.event.get():
@@ -172,11 +146,6 @@ while not exit:
         else:
             bullets.pop(bullets.index(bullet))
 
-    for bullet in bullets:
-        if bullet.x < 500 and bullet.x > 0:
-            bullet.x += bullet.vel 
-        else:
-            bullets.pop(bullets.index(bullet))
 
     keys = pygame.key.get_pressed()
 
@@ -188,26 +157,17 @@ while not exit:
         if len(bullets) < 5:
             bullets.append(shoot(round(man.x + man.width // 2),round(man.y + man.height // 2), 6, (0,0,0), facing))
 
-    if keys[pygame.K_SPACE]:
-        if man.left:
-            facing = -1
-        else:
-            facing = 1
-        if len(bullets) < 5:
-            bullets.append(shoot(round(man.x + man.width // 2),round(man.y + man.height // 2), 6, (0,0,0), facing))
     if keys[pygame.K_LEFT] and man.x > man.vel:
         man.x -= man.vel
         man.left = True
         man.right = False
         man.standing = False
-    elif keys[pygame.K_RIGHT] and man.x < 500-man.width-man.vel:
+    elif keys[pygame.K_RIGHT] and man.x < 500 - man.width - man.vel:
         man.x += man.vel
         man.right = True
         man.left = False
         man.standing = False
-        man.standing = False
     else:
-        man.standing = True
         man.standing = True
         man.walkCount = 0
     if not(man.isJump):
@@ -218,11 +178,14 @@ while not exit:
             man.walkCount = 0
     else:
         if man.jumpCount >= -10:
-            man.y -= (man.jumpCount * abs(man.jumpCount)) * 0.25
+            neg = 1
+            if man.jumpCount < 0:
+                neg = -1
+            man.y -= (man.jumpCount ** 2) * 0.5 * neg
             man.jumpCount -= 1
-        else: 
-            man.jumpCount = 10
+        else:
             man.isJump = False
+            man.jumpCount = 10
 
     redrawScreen()
 
